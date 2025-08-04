452e47abab0a89c893be9d5d5534918c321603a6
Jacob Sutherland <jadau1313@gmail.com>
2025-08-03 23:01:48 -0700

    removed xg boost requirement over file size

--- test_model_load.py
+++ test_model_load.py
@@ -1,8 +1,8 @@
 import joblib
 
 try:
-    model = joblib.load('app/models/xgboost_sleep_model.pkl')
-    #model = joblib.load('app/models/lgbm_sleep_model.pkl')
+    #model = joblib.load('app/models/xgboost_sleep_model.pkl')
+    model = joblib.load('app/models/lgbm_sleep_model.pkl')
     print("Model successfully loaded")
 except Exception as e:
     print("Error loading model", e)
